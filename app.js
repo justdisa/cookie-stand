5a2eb1f5d43cc02c10fa98d6ee4065241ebd2038
Disa <justdisa@gmail.com>
2017-01-09 22:24:31 -0800

    fixed problem with js reference

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+'use strict';
 
+var stores = ['Beacon Hill', 'Columbia City', 'SoDo', 'Ballard', 'Alki', 'Shoreline'];
+var sales = [328.45, 99.04, 12.11, 176.19, 180.76, 85.22];
+var customers = [412, 7, 2, 70, 112, 36];
+
+var storeList = document.getElementById('my-list');
+
+for(var index = 0; index < stores.length; index++){
+  console.log(stores[index]);
+  var listElement = document.createElement('li');
+  listElement.setAttribute('class', 'storeList-item');
+  listElement.textContent = stores[index] + ' - Index: ' + index;
+  storeList.appendChild(listElement);
+}
