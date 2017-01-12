e5b5df7882ac290cef637864565da97dd22557d9
Disa <justdisa@gmail.com>
2017-01-12 06:25:15 -0800

    adding code built while running a really high fever

--- app.js
+++ app.js
@@ -1,153 +1,82 @@
 'use strict';
 
+//declare and initialize variables//
+var storesList = [];
 var times = ['6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM', 'Total: '];
-
-var results = [];
-
-function cookiesSum() {
-  var sum = 0;
-  for (var j = 0; j < this.results.length; j++) {
-    sum = sum + this.results[j];
-  }
-  this.results.push(sum);
-  console.log(sum);
+var salesGrandTotal = 0;
+var salesTableNode;
+var salesTodayTotal = [];
+for (var i = 0; i < times.length - 1; i++) {
+  salesTodayTotal[i] = 0;
 }
 
-function cookiesPerHour() {
-  for(var i = 0; i < times.length - 1; i++){
-    var randomNumber = Math.floor(Math.random() * (this.maxCustomers - minCustomers) + minCustomers);
-    console.log(randomNumber);
-    var numCookies = Math.floor (randomNumber * this.avgCookies);
-    console.log(numCookies);
-    results.push(numCookies);
-  }
-}
-
-function Store(name, minCustomers, maxCustomers, avgCookies, results, cookiesPerHour){
+function Store(name, timeOpen, timeClosed, minCustomers, maxCustomers, avgCookies, result, cookiesPerHour, cookiesSoldTotal) {
   this.name = name;
+  this.timeOpen = timeOpen;
+  this.timeClosed = timeClosed;
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
   this.avgCookies = avgCookies;
-  this.results = results;
-  this.cookiesPerHour = cookiesPerHour;
+  this.result = result;
+  this.cookiesPerHour = cookiesPerHour();
+  this.cookiesSoldTotal = cookiesSoldTotal();
+};
+
+Store.prototype.checkSales = function () {
+  var hoursOpen = this.timeClosed - this.timeOpen;
+  console.log('Open for ' + hoursOpen + ' hours today.');
+  for (var i = 0; i < hoursOpen; i++) {
+    console.log('Hours elapsed: ' + i);
+    var result = Math.ceil(this.randomCustomers() * this.averageCookiesPerSale);
+    console.log('At ' + (i + this.timeOpen) + '.00' + result + ' cookies were sold.');
+    this.cookiesSoldToday += result;
+    this.cookiesSoldHourly.push[result];
+    console.log('Hourly breakdown: ' + this.cookiesSoldHourly);
+    console.log('Cookies sold in all stores this hour: ' + this.cookiesSoldHourly);
+    salesTodayTotal[i] += result; // you are adding result in a lot of places. Where do you need it? what is salesTodayTotal[i]?//
+    console.log('Total cookies sold so far today: ' + salesGrandTotal); // what type is salesGrandTotal? What are you trying to print here? //
+  }
+};
+
+var firstAndPike = new Store ('First and Pike', 23, 65, 6.3, result, cookiesPerHour);
+var seatacAirport = new Store ('Seatac Airport', 3, 24, 1.2, result, cookiesPerHour);
+var seattleCenter = new Store ('Seattle Center', 11, 38, 3.7, result, cookiesPerHour);
+var capitolHill = new Store ('Capitol Hill', 20, 38, 2.3, result, cookiesPerHour);
+var alki = new Store ('Alki', 2, 16, 4.6, result, cookiesPerHour);
+
+function tableCreate() {
+//body reference
+  var body = document.getElementsByTagName('body')[0];
+// create elements <table> and a <tbody>
+  var tbl     = document.createElement('table');
+  var tblBody = document.createElement('tbody');
+
+        // cells creation
+  for (var i = 0; i < result.length; i++) { // how many times do you want to repeat this? // //like that?// you want a row for each result? //
+            // table row creation
+    var row = document.createElement('tr');
+
+    for (var j = 0; j < storesList.length; j++) { // how many times do you want ot repeat this?
+        // create element <td> and text node
+        //Make text node the contents of <td> element
+        // put <td> at end of the table row
+      var cell = document.createElement('td');
+      var cellText = document.createTextNode('cell is row ' + j + ', column ' + i); //what do you want your text to be? //
+
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+            //row added to end of table body
+    tblBody.appendChild(row);
+  }
+        // append the <tbody> inside the <table>
+  tbl.appendChild(tblBody);
+        // put <table> in the <body>
+  body.appendChild(tbl);
+        // tbl border attribute to
+  tbl.setAttribute('border', '2');
 }
 
-var firstAndPike = new Store ('First and Pike', 23, 65, 6.3, results, cookiesPerHour);
-var seatacAirport = new Store ('Seatac Airport', 3, 24, 1.2, results, cookiesPerHour);
-var seattleCenter = new Store ('Seattle Center', 11, 38, 3.7, results, cookiesPerHour);
-var capitolHill = new Store ('Capitol Hill', 20, 38, 2.3, results, cookiesPerHour);
-var alki = new Store ('Alki', 2, 16, 4.6, results, cookiesPerHour);
-
-console.log(firstAndPike);
-console.log(seatacAirport);
-console.log(seattleCenter);
-console.log(capitolHill);
-console.log(alki);
-//
-//
-// var alki = {
-//   minCustomers: 2,
-//   maxCustomers: 16,
-//   avgCookies: 4.6,
-//   results: [],
-//   cookiesPerHour: function() {
-//     for(var i = 0; i < times.length - 1; i++){
-//       var randomNumber = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers);
-//       console.log(randomNumber);
-//       var numCookies = Math.floor (randomNumber * this.avgCookies);
-//       console.log(numCookies);
-//       this.results.push(numCookies);
-//     }
-//   },
-//   cookiesSum: function () {
-//     var sum = 0;
-//     for (var j = 0; j < this.results.length; j++) {
-//       sum = sum + this.results[j];
-//     }
-//     this.results.push(sum);
-//     console.log(sum);
-//   }
-// };
-//
-// //calling objects, functions, and DOM code by store//
-// //firstAndPike store
-// firstAndPike.cookiesPerHour();
-// firstAndPike.cookiesSum();
-//
-// var myList = document.getElementById('firstandpike');
-// myList.setAttribute('class', 'cookie-list');
-//
-// for (var i = 0; i < firstAndPike.results.length; i++){
-//   var listEl = document.createElement('li');
-//   listEl.textContent = times[i] + ' ' + firstAndPike.results[i];
-//   if (i === firstAndPike.results.length - 1){
-//     listEl.setAttribute('class', 'cookie-total');
-//   }
-//   myList.appendChild(listEl);
-// }
-//
-// //seatacAirport store
-// seatacAirport.cookiesPerHour();
-// seatacAirport.cookiesSum();
-//
-// var myList = document.getElementById('seatacairport');
-// myList.setAttribute('class', 'cookie-list');
-//
-// for (var i = 0; i < seatacAirport.results.length; i++){
-//   var listEl = document.createElement('li');
-//   listEl.textContent = times[i] + ' ' + seatacAirport.results[i];
-//   if (i === seatacAirport.results.length - 1){
-//     listEl.setAttribute('class', 'cookie-total');
-//   }
-//   myList.appendChild(listEl);
-// }
-//
-// //seattleCenter store
-// seattleCenter.cookiesPerHour();
-// seattleCenter.cookiesSum();
-//
-// var myList = document.getElementById('seattlecenter');
-// myList.setAttribute('class', 'cookie-list');
-//
-// for (var i = 0; i < seattleCenter.results.length; i++){
-//   var listEl = document.createElement('li');
-//   listEl.textContent = times[i] + ' ' + seattleCenter.results[i];
-//   if (i === seattleCenter.results.length - 1){
-//     listEl.setAttribute('class', 'cookie-total');
-//   }
-//   myList.appendChild(listEl);
-// }
-//
-// //capitolHill store
-// capitolHill.cookiesPerHour();
-// capitolHill.cookiesSum();
-//
-// var myList = document.getElementById('capitolhill');
-// myList.setAttribute('class', 'cookie-list');
-//
-// for (var i = 0; i < capitolHill.results.length; i++){
-//   var listEl = document.createElement('li');
-//   listEl.textContent = times[i] + ' ' + capitolHill.results[i];
-//   if (i === capitolHill.results.length - 1){
-//     listEl.setAttribute('class', 'cookie-total');
-//   }
-//   myList.appendChild(listEl);
-// }
-//
-// //alki store
-// alki.cookiesPerHour();
-// alki.cookiesSum();
-//
-// var myList = document.getElementById('alki');
-// myList.setAttribute('class', 'cookie-list');
-//
-// for (var i = 0; i < alki.results.length; i++){
-//   var listEl = document.createElement('li');
-//   listEl.textContent = times[i] + ' ' + alki.results[i];
-//   if (i === alki.results.length - 1){
-//     listEl.setAttribute('class', 'cookie-total');
-//   }
-//   myList.appendChild(listEl);
-// }
-//
-////Better.
+//dmDiana--code comes in pretty colors.//
+//dmI need to have a number randomize in one place//
+//coding is all pretty. Don't know what is happening here//
